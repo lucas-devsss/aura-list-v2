4ac437b3719bda4fe803419f9e10f9f6fbadbfd8
Caio Lucas <clucaszrtmain@gmail.com>
2026-03-12 17:46:16 +0000

    feat: Adiciona o Header e Formulário, e adiciona um estado para o nome da Task

--- aura-list-v2/src/App.tsx
+++ aura-list-v2/src/App.tsx
@@ -2,11 +2,32 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState()
+  const [nameTask, setNameTask] = useState<string>("")
+
+
+  function handleSubmitForm(e: any): void{
+    e.preventDefault()
+    console.log("absdba")
+  }
 
   return (
     <>
-     <h1>oi</h1>
+    <header className="task-app__header">
+        <div className="task-app__title-wrapper">
+            <h1>Aura List</h1>
+        </div>
+    </header>
+
+    <form className="task-form" onSubmit={handleSubmitForm}>
+            <input className='task-form__input' value={nameTask} onChange={(e) => setNameTask(e.currentTarget.value)} type="text" aria-label="Adicione o nome de sua tarefa"/>
+            <fieldset className="task-form__priority">
+                <legend className="task-form__priority-legend">Prioridade Tarefa</legend>
+                <label htmlFor="urgente"><input type="radio" name="prioridade" id="urgente" className="task-form__priority-input task-form__priority-input--urgente"/>Urgente</label>
+                <label htmlFor="importante"><input type="radio" name="prioridade" id="importante" className="task-form__priority-input task-form__priority-input--importante"/>Importante</label>  
+                <label htmlFor="rotineira"><input type="radio" name="prioridade" id="rotineira" className="task-form__priority-input task-form__priority-input--rotineira"/>Rotineira</label>
+            </fieldset>
+            <input type="submit" className="task-form__button" value="Adicionar tarefa"></input>
+      </form>
     </>
   )
 }
