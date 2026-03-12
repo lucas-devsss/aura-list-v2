efe57d43a053ff46d5ccd7fb633d682a8596351d
Caio Lucas <clucaszrtmain@gmail.com>
2026-03-12 18:19:33 +0000

    feat: Adiciona a funcionalidade de deletar tarefa atualizando o estado com o método Filter e cria uma Interface para lidar com a estrutura de objeto das tasks

--- aura-list-v2/src/App.tsx
+++ aura-list-v2/src/App.tsx
@@ -1,9 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 
+interface Task{
+  id: number,
+  name: string
+}
+
+
 function App() {
   const [nameTask, setNameTask] = useState<string>("")
-  const [tasks, setTasks] = useState<string[]>([])
+  const [tasks, setTasks] = useState<Task[]>([])
 
 
   function handleSubmitForm(e: any): void{
@@ -11,7 +17,7 @@ function App() {
     if(nameTask === ""){
       return
     }
-    setTasks(e => [...e, nameTask])
+    setTasks(e => [...e, {id: tasks.length + 1, name: nameTask}])
     setNameTask("")
     console.log(tasks)
   }
@@ -37,7 +43,7 @@ function App() {
 
     <ul>
       {tasks.map(a => {
-        return <li key={a}>{a}</li>
+        return <li key={a.id}>{a.name} <svg onClick={() => setTasks(tasks.filter(t => a !== t))} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-delete-icon lucide-delete"><path d="M10 5a2 2 0 0 0-1.344.519l-6.328 5.74a1 1 0 0 0 0 1.481l6.328 5.741A2 2 0 0 0 10 19h10a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2z"/><path d="m12 9 6 6"/><path d="m18 9-6 6"/></svg></li>
         })}
     </ul>
     </>
