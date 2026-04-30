66ca990708b41baf29e29ace70c7b9689364f739
Caio Lucas <clucaszrtmain@gmail.com>
2026-04-30 16:54:35 -0300

    feat: Cria uma função para resgatar um item do localstorage

--- src/storage/taskStorage.ts
+++ src/storage/taskStorage.ts
@@ -4,3 +4,11 @@ import type { Task } from "../types/taskType";
 export function saveLocalStorage(name: string, item: Task[]){
     localStorage.setItem(name, JSON.stringify(item))
 }
+
+export function getLocalStorage(name: string){
+    const itemStorage = localStorage.getItem(name)
+    if(itemStorage){
+        return JSON.parse(itemStorage)
+    }
+    return []
+}
