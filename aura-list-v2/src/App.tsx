8d5cd33d9ba09ec0cdbd62f7d231190045bf4df5
Caio Lucas <clucaszrtmain@gmail.com>
2026-04-29 23:17:28 -0300

    refactor: remove a lógica de App e desestrutura a lógica de useTasks

--- aura-list-v2/src/App.tsx
+++ aura-list-v2/src/App.tsx
@@ -1,60 +1,14 @@
-import {useState } from 'react'
 import './App.css'
-import type { Task } from './types/taskType'
 import RenderTasks from './components/RenderTasks'
 import FormTask from './components/FormTask'
-import { difficultyWeight } from './types/taskType'
+import { useTasks } from './hooks/useTasks'
+
+
 
 function App() {
-  const [nameTask, setNameTask] = useState<string>("")
-  const [tasks, setTasks] = useState<Task[]>([])
-  const [modal, setModal] =useState<boolean>(false)
-  const [taskEdit, setTaskEdit] = useState<Task | null> (null)
-  const [taskDifficulty, setTaskDifficulty] = useState<"easy" | "medium" | "hard">("easy")
-
-
-
-
-  function handleSubmitForm(): void{
-    if(nameTask === ""){
-      return
-    }
-    setTasks(e => [...e, {id: tasks.length + 1, name: nameTask, difficulty: taskDifficulty}].sort((a, b) => difficultyWeight[b.difficulty] - difficultyWeight[a.difficulty]))
-    setNameTask("")
-    console.log(taskDifficulty)
-  }
-
-  function handleDeleteTask(id: number): void{
-    setTasks(prev => prev.filter(task => task.id !== id))
-    setModal(false)
-  }
-
-  function handleEditTask(idTask: number, nameTask: string, difficulty: "easy" | "medium" | "hard"): void{
-    setTaskEdit({id: idTask, name: nameTask, difficulty: difficulty})
-    setModal(true)
-  }
-
-  function handleChangeTask(nameTask: string): void{
-    setTaskEdit({id: taskEdit!.id, name: nameTask, difficulty: taskEdit!.difficulty})
-  }
-
-  function handleUpdateTasks(tasks: Task[]){
-    setTasks(tasks)
-    setModal(false)
-  }
-
-  function handleCloseModal(): void{
-    setModal(false)
-  }
-
-  function handleSaveNameTask(e: string){
-    setNameTask(e)
-  }
-
-  function handleSaveDifficulty(e: "easy" | "medium" | "hard"): void{
-    setTaskDifficulty(e)
-  }
-
+  
+  const { handleSubmitForm, handleDeleteTask, handleEditTask, handleChangeTask, handleUpdateTasks, handleCloseModal, handleSaveNameTask, handleSaveDifficulty, tasks, nameTask, modal, taskEdit } = useTasks()
+  
   return (
     <>
     <FormTask handleSubmitForm={handleSubmitForm} nameTask={nameTask} handleSaveNameTask={handleSaveNameTask} handleSaveDifficulty={handleSaveDifficulty}/>
