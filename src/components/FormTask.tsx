9fb7cdd085d8c5e734ea6ebbe3be1469d7aa7ada
Caio Lucas <clucaszrtmain@gmail.com>
2026-05-07 16:12:26 -0300

    style: Adiciona o estilo do FormTask e faz as mudanças das classes dos elementos

--- src/components/FormTask.tsx
+++ src/components/FormTask.tsx
@@ -4,22 +4,22 @@ function FormTask({handleSubmitForm, nameTask, handleSaveNameTask, handleSaveDif
     return(
         <>
         <header>
-            <h1 className={styles.title}>Aplicação Tarefas</h1>
+            <h1 className={styles.title}>Aura List</h1>
         </header>
 
-    <form className="task-form" onSubmit={(e) =>{
+    <form className={styles.form} onSubmit={(e) =>{
         e.preventDefault()
         handleSubmitForm()
     }}>
-        <label htmlFor="task-form__input">Adicione sua tarefa</label>
-            <input className='task-form__input' id="task-form__input" value={nameTask} onChange={(e) => handleSaveNameTask(e.currentTarget.value)} type="text" aria-label="Adicione o nome de sua tarefa"/>
-            <fieldset className="task-form__priority">
-                <legend className="task-form__priority-legend">Prioridade Tarefa</legend>
-                <label htmlFor="dificil"><input type="radio" name="dificuldade" id="dificil" onChange={e => handleSaveDifficulty(e.currentTarget.value)} value="hard"  className="task-form__priority-input task-form__priority-input--urgente" />Difícil</label>
-                <label htmlFor="medio"><input type="radio" name="dificuldade" id="medio" onChange={e => handleSaveDifficulty(e.currentTarget.value)} value="medium"  className="task-form__priority-input task-form__priority-input--importante"/>Médio</label>  
-                <label htmlFor="facil"><input type="radio" name="dificuldade" id="facil" onChange={e => handleSaveDifficulty(e.currentTarget.value)} value="easy"  className="task-form__priority-input task-form__priority-input--rotineira" defaultChecked/>Fácil</label>
+        <label className={styles.label} htmlFor="task-form__input">Adicione sua tarefa</label>
+            <input className={styles.input} id="task-form__input" value={nameTask} onChange={(e) => handleSaveNameTask(e.currentTarget.value)} type="text" aria-label="Adicione o nome de sua tarefa" placeholder="Adicione sua tarefa"/>
+            <fieldset className={styles["difficulty-container"]}>
+                <legend className={styles.legend}></legend>
+                <label htmlFor="dificil" className={styles["badge-hard"]}><input type="radio" className={styles["input-radio"]} name="dificuldade" id="dificil" onChange={e => handleSaveDifficulty(e.currentTarget.value)} value="hard"/>Difícil</label>
+                <label htmlFor="medio" className={styles["badge-medium"]}><input type="radio" className={styles["input-radio"]} name="dificuldade" id="medio" onChange={e => handleSaveDifficulty(e.currentTarget.value)} value="medium"/>Médio</label>  
+                <label htmlFor="facil" className={styles["badge-easy"]}><input type="radio" className={styles["input-radio"]} name="dificuldade" id="facil" onChange={e => handleSaveDifficulty(e.currentTarget.value)} value="easy" defaultChecked/>Fácil</label>
             </fieldset>
-            <input type="submit" className="task-form__button" value="Adicionar tarefa"/>
+            <input type="submit" className={styles["btn-add-task"]} value="Adicionar tarefa"/>
     </form>
     </>
  )
